887a23fa204c4cc3a2960d6b44d64bc242dc314d
Soham Goswami <sohamgoswami@duck.com>
2025-12-06 15:17:45 +0530

    Fixed site and base settings astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    site: "https://hotel-veda-royale.web.app/",
+    base: "/",
     vite: {
         plugins: [tailwindcss()],
     }
